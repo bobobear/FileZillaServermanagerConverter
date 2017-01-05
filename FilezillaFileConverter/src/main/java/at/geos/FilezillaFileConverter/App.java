825a8faf77ce544cf5dad4bcf4bc5c5f6c9e773e
geos <geraldoswald1@gmx.at>
2017-01-05 15:20:40 +0100

    Initial checkin

---
+++ FilezillaFileConverter/src/main/java/at/geos/FilezillaFileConverter/App.java
@@ -0,0 +1,13 @@
+package at.geos.FilezillaFileConverter;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" ); 
+    }
+}
